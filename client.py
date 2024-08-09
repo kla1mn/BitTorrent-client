0d954991123a7a2f3422ad5608a17deb9aaeb1f3
Konstantin Kolesnikov <kolesnikov.kons@gmail.com>
2024-08-09 22:03:33 +0500

    Update .gitignore

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ from file_saver import FileSaver
 
 
 def main():
-    file_name = "amanita.torrent"
+    file_name = "Torrent files/amanita.torrent"
     data = torrent.Torrent(file_name)
     file_saver = FileSaver(data)
 
